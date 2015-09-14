cbdd5dbed2c287de4782bd25f70f78d5f79a770e
NaveedAhmed92 <naveedahmed2292@gmail.com>
2015-09-14 15:53:16 +0500

    modified commandline arguments...

--- src/main/java/com/tg/TwitterGrapher.java
+++ src/main/java/com/tg/TwitterGrapher.java
@@ -1,112 +1,32 @@
 package com.tg;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Starter;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.StaticHandler;
-import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
-
-import java.util.Map;
-
-import com.tg.factory.GraphFactory;
-import com.tg.graph.Graph;
 
 /**
  * Hello world!
  *
  */
-public class TwitterGrapher extends AbstractVerticle 
+public class TwitterGrapher 
 {
-	String path = null;
+	private static final String VERTICLE_CLASS = "com.tg.GraphServer";
+	
     public static void main( String[] args )
     {
+    	args = ( args.length > 1 && args[0].equals("run") ) ? args : getCommandLineArguments(args);
+    	
     	 Starter.main(args);
     }
     
-	@Override
-	public void start(Future<Void> startFuture) throws Exception {
-		// TODO Auto-generated method stub
-		super.start(startFuture);
-		
-		 final ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
-		 final ThymeleafTemplateEngine engine1 = ThymeleafTemplateEngine.create();
-		    // To simplify the development of the web components we use a Router to route all HTTP requests
-		    // to organize our code in a reusable way.
-		    
-		    final Router mainrouter = Router.router(vertx);
-		    final Router infoRouter = Router.router(vertx);
-		    JsonObject config;
-		    config = context.config();
-		    Map<String,Object> settings = config.getMap();
-	    	 JsonObject lso = (JsonObject) settings.get("layout_settings");
-	    	 Map<String, Object> ls = lso.getMap();
-	    	 String bk_color = ls.get("bk_color").toString();
-	    	 JsonObject ips = (JsonObject) ls.get("ips");
-	    	 Map<String, Object> ip = ips.getMap();
-	    	 JsonObject aso = (JsonObject)settings.get("app_settings");
-	    	 Map<String, Object> as = aso.getMap();
-	    	 JsonObject gso = (JsonObject) settings.get("graph_settings");
-	    	 Map<String, Object> gs = gso.getMap();
-	    	  String getdata = as.get("getdata").toString();
-	    	  System.out.println("value of getdata is: "+getdata);
-	    	 /*if(getdata.equals("file")){
-	    		  path = as.get("input_file").toString();
-	    	 }*/
-	    	 int Port = Integer.parseInt(as.get("port").toString());
-	    	 
-		     mainrouter.route("/templates/*").handler(StaticHandler.create("com/tg/templates").setCachingEnabled(false));
-		     
-		     infoRouter.route("/").handler(ct ->{
-		    	 engine.render(ct, "com/tg/templates/info.html", res -> {
-				        if (res.succeeded()) {
-				          ct.response().putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
-				          .end(res.result());
-				        } else { 
-				          ct.fail(res.cause()); 
-				        }
-				      });
-		     });
-		     
-		     mainrouter.get("/graph").method(HttpMethod.GET).handler(ctx -> {
-		    	 
-		     ls.put("sv", getSearchValue(ctx.request().absoluteURI()));
-		     ctx.put("color",bk_color );	 
-		     ctx.put("welcome", "Hi there!");
-		     ctx.put("graph_settings", gs);
-		     
-		     //. generating graph...
-		     String type = "sigmaGraph";
-	    	 GraphFactory graphFactory = new GraphFactory();
-	    	 Graph graph = graphFactory.getGraph(type, ls);
-	    	 ctx.put("nodes", graph);
-		     
-		     // and now delegate to the engine to render it.
-		     engine.render(ctx, "com/tg/templates/index.html", res -> {
-		    	 
-		        if (res.succeeded()) {
-		        	 
-		         ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "text/html")
-		         .end(res.result());
-		         
-		        } else {
-		          ctx.fail(res.cause()); 
-		        }
-		      });
-		    });
-		     mainrouter.mountSubRouter("/", infoRouter);
-	    vertx.createHttpServer().requestHandler(mainrouter::accept).listen(Port);
-	}
-
-	private String getSearchValue(String url)
-	{
-		String [] sv = url.split("=");
-		if(sv.length == 2)
-		return sv[1];
-		else
-			return "hyundai";
-	}
+    public static String[] getCommandLineArguments(String[] args){
+    	String[] commandLineArguments = new String[args.length+2];
+    	commandLineArguments[0] = "run";
+    	commandLineArguments[1] = VERTICLE_CLASS;
+    	for(int i = 0; i< args.length; i++){
+    		commandLineArguments[i+2] = args[i];
+    		
+    	}
+    	return commandLineArguments;
+    }
+    
+	
 }
