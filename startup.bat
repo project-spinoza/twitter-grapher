b4f999b77e17163b55cc58602056f11b1b1fe0ad
kowalski100 <ijaz.a100@yahoo.com>
2015-11-11 15:28:07 -0800

    startup batch script

---
+++ startup.bat
@@ -0,0 +1,83 @@
+@echo off
+
+set numArgs=0
+for %%x in (%*) do Set /A numArgs+=1
+
+IF %1 == start (
+
+	goto run
+
+) ELSE IF %1 == stop (
+
+	goto stop
+
+) ELSE IF %1 == status (
+
+	goto status
+
+) ELSE (
+
+	echo Unknown arguments' %*
+)
+goto eof
+
+
+
+:run
+if not exist "logs" mkdir logs
+	start /B java -jar target/twitter-grapher-v1.0-fat.jar -conf config.json >.\logs\logs.txt 2>&1
+
+   "%JAVA_HOME%/bin/jps.exe" -v > .\logs\processes.txt
+
+   FOR /F "tokens=1,2" %%a IN (.\logs\processes.txt) DO (
+
+	   IF %%b == twitter-grapher-v1.0-fat.jar (
+
+	   		echo Server running under PID: %%a
+	   		echo %%a > .\logs\pid.dat
+	   )
+
+)
+goto eof
+
+
+:stop
+IF EXIST .\logs\pid.dat (
+
+		  set /p pid=< .\logs\pid.dat  
+		  IF "%pid%" == "" (
+		  		echo No server running.
+		  )ELSE (
+
+		  	taskkill /f /pid %pid%
+		  	IF EXIST .\logs\pid.dat del /F .\logs\pid.dat
+		  	echo Server stoped successfully.
+
+		  )
+
+) ELSE (
+
+		echo No server running.
+)
+goto eof
+
+
+:status 
+IF EXIST .\logs\pid.dat (
+
+		  set /p pid=< .\logs\pid.dat  
+		  IF "%pid%" == "" (
+		  		echo No server running.
+		  )ELSE (
+		  	echo Server running under PID: %pid%
+
+		  )
+
+) ELSE (
+
+		echo No server running.
+)
+
+
+:eof
+Exit /b
