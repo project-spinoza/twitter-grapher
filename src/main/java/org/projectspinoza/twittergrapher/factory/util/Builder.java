735b79568673aaa46f0a293848a578ae83be4d4d
kowalski100 <ijaz.a100@yahoo.com>
2015-12-29 16:04:35 -0800

    Builder class code refinement

--- src/main/java/org/projectspinoza/twittergrapher/factory/util/Builder.java
+++ src/main/java/org/projectspinoza/twittergrapher/factory/util/Builder.java
@@ -4,6 +4,7 @@ import io.vertx.core.json.JsonObject;
 
 import java.awt.Color;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 import org.gephi.data.attributes.api.AttributeColumn;
@@ -35,114 +36,111 @@ import de.uni_leipzig.informatik.asv.gephi.chinesewhispers.ChineseWhispersCluste
 
 public class Builder {
 
-	@SuppressWarnings("unchecked")
 	public static Graph build(boolean IS_DIRECTED, Map<String, Object> settings) {
 		
 		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
 		pc.newProject();
 		Workspace workspace = pc.getCurrentWorkspace();
-
+		
+		//loaders
 		AttributeModel attributeModel = Lookup.getDefault().lookup(AttributeController.class).getModel();
 		GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getModel();
 		PreviewModel model = Lookup.getDefault().lookup(PreviewController.class).getModel();
 		ImportController importController = Lookup.getDefault().lookup(ImportController.class);
 //		FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
 		RankingController rankingController = Lookup.getDefault().lookup(RankingController.class);
-		PageRank pr = new PageRank();
-		double epsilon =0.001 ;
-		double probability = 0.85;
-		Graph getgraph=null;
+		
+		int pagerankthreshhold = Integer.parseInt(settings.get("prt").toString());
+		Double neighborCountRange = Double.parseDouble(settings.get("neighborcountrange").toString());
+		int nodeCentrality = (Integer) settings.get("nct");
+		double Nodecentralitythreshhold = (double) nodeCentrality / 100;
 		String coloumnValue = (String) settings.get("nsb");
 		String columnname = "NeighborCount";
-//		double range = 0.0;
-		int pagerankthreshhold = Integer.parseInt(settings.get("prt").toString());
-		Double neighborcountrange = Double.parseDouble(settings.get("neighborcountrange").toString());
-		int nodecentrality = (Integer) settings.get("nct");
-		double Nodecentralitythreshhold = (double) nodecentrality / 100;
-		double prthreshhold = (double) pagerankthreshhold / 100;
-	
+		
 		// Getting layout Options
 		JsonObject layoutAlgoJson = new JsonObject(settings.get("la").toString());
 		Map<String, Object> layoutSettings = layoutAlgoJson.getMap();
-		JsonObject sourcesCredJson = (JsonObject) ((Map<String, Object>) settings.get("settings")).get("sources_cred");
-		String inputFile = sourcesCredJson.getString("file");
-		String dataSource = ((Map<String, Object>) settings.get("settings")).get("source_selected").toString();
 		
-		// Import file
 		Container container = null;
 		try{
-			if (DataSourceType.contains(dataSource)) {
-				container = (new DataSourceImporter()).importDataSource(settings);
-				if (container == null){
-					System.out.println("Error generating graph from "+inputFile);
-				}
-			} else if (dataSource.equals("graphfile")){
-				File file = new File(inputFile);
-				container = importController.importFile(file);
-				if (container == null){
-					System.out.println("No graph found in "+inputFile+". Make sure you are using correct graph file format.");
-				}
-			}else {
-				System.out.println ("Unsupported file format.");
-				return null;
-			}
+			container = generateGraph(importController, settings);
 		} catch (Exception ex) {
-			
 			ex.printStackTrace();
 			return null;
 		}
 
 		// Append imported data to GraphAPI
 		importController.process(container, new DefaultProcessor(), workspace);
-		Graph graph = getgraph(graphModel,IS_DIRECTED,getgraph); 
-		calculatePageRank(graphModel, attributeModel, pr, IS_DIRECTED, epsilon, probability);
+		Graph graph = getgraph(graphModel,IS_DIRECTED,null); 
+		
+		calculatePageRank(graphModel, attributeModel, IS_DIRECTED);
 		addingcolumntoattributemodel(graph , attributeModel, columnname);
 		ChineseWhispersClusterer cwc = new ChineseWhispersClusterer();
 		cwc.execute(graphModel);
 
-		// Run Layout for 100 passes - The layout always takes the
 		int iterations = layoutSettings.containsKey("it") ? Integer.parseInt(layoutSettings.get("it").toString()) : 50;
 		setLayouts(graphModel, layoutSettings, iterations);
 		
 		GraphDistance distance = new GraphDistance();
 		getcentrality(graphModel, attributeModel, distance , true);
 
-		// Sorting Nodes based On some column Value and Removing Percentage
-		// Nodes Based On Column Value
 		if (pagerankthreshhold != 0) {
 			columnname = "pageranks";
 			AttributeColumn column = attributeModel.getNodeTable().getColumn(columnname);
-			graph = Filters.RemovePercentageNodes(graph, column, prthreshhold,columnname);
+			double prThreshHold = (double) pagerankthreshhold / 100;
+			graph = Filters.RemovePercentageNodes(graph, column, prThreshHold ,columnname);
 		}
-		if (nodecentrality != 0) {
+		if (nodeCentrality != 0) {
 			columnname = "Betweenness Centrality";
 			AttributeColumn column = attributeModel.getNodeTable().getColumn(GraphDistance.BETWEENNESS);
 			graph = Filters.RemovePercentageNodes(graph, column,Nodecentralitythreshhold, columnname);
 		}
 		
-		if (neighborcountrange != 0) {
+		if (neighborCountRange != 0) {
 			columnname = "NeighborCount";
 			AttributeColumn column = attributeModel.getNodeTable().getColumn(columnname);
-			graph = Filters.RemovePercentageNodes(graph, column,neighborcountrange/100, columnname);
+			graph = Filters.RemovePercentageNodes(graph, column, neighborCountRange/100, columnname);
 		}
-		
 		rankingColorByDegree(rankingController);
 		
 		// Rank Size By Page Rank Or Node Centrality
 		if (coloumnValue.contains("nc")) {
-
 			Filters.RankSize("Betweenness Centrality", attributeModel,rankingController);
 		} else if (coloumnValue.contains("pr")) {
-			
 			Filters.RankSize("pageranks", attributeModel, rankingController);
 		}
-		
 		// Preview
-		   getpreview(model);
-		   System.out.println("Nodes-------->:"+graph.getNodeCount());
-		   System.out.println("Edges-------->:"+graph.getEdgeCount());
+		getpreview(model);
+		System.out.println("Nodes-------->:"+graph.getNodeCount());
+		System.out.println("Edges-------->:"+graph.getEdgeCount());
 
 		return graph;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Container generateGraph(ImportController importController, Map<String, Object> settings) throws FileNotFoundException{
+		
+		JsonObject sourcesCredJson = (JsonObject) ((Map<String, Object>) settings.get("settings")).get("sources_cred");
+		String inputFile = sourcesCredJson.getString("file");
+		String dataSource = ((Map<String, Object>) settings.get("settings")).get("source_selected").toString();
+		Container container = null;
+		
+		if (DataSourceType.contains(dataSource)) {
+			container = (new DataSourceImporter()).importDataSource(settings);
+			if (container == null){
+				System.out.println("Error generating graph from "+inputFile);
+			}
+		} else if (dataSource.equals("graphfile")){
+			File file = new File(inputFile);
+			container = importController.importFile(file);
+			if (container == null){
+				System.out.println("No graph found in "+inputFile+". Make sure you are using correct graph file format.");
+			}
+		}else {
+			System.out.println ("Unsupported file format.");
+		}
+		
+		return container;
 	}
 	
 	public static Graph getgraph(GraphModel graphModel, Boolean IS_DIRECTED, Graph graph){
@@ -151,7 +149,8 @@ public class Builder {
 		return graph;
 	}
 	
-	public static void calculatePageRank(GraphModel graphModel, AttributeModel attributeModel,PageRank pr, Boolean IS_DIRECTED, double epsilon, double probability){
+	public static void calculatePageRank(GraphModel graphModel, AttributeModel attributeModel, Boolean IS_DIRECTED){
+		PageRank pr = new PageRank();
 		pr.setDirected(IS_DIRECTED);
 		pr.setEpsilon(0.001);
 		pr.setProbability(0.85);
